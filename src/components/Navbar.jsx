29635f6ff6c68575c91524aac928833c58be7718
Amit Bharti <136784383+9Amit@users.noreply.github.com>
2025-05-06 08:35:13 +0530

    m

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+</style>;
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md px-6 py-4">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-blue-600">
+        <Link to="/" className="text-2xl font-relaway font-bold text-blue-600">
           JobFinder
         </Link>
 
